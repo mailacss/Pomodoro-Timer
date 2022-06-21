3a582b31eb3902f67701fa880abc37c2a1ca4040
Maila Cardoso <cardmaila19@gmail.com>
2022-06-21 04:01:22 -0300

    menu principal

---
+++ script.js
@@ -0,0 +1,8 @@
+function enviar(){
+    var time = document.getElementById("timeX").value;
+    var pausaCurta = document.getElementById("shortPause").value;
+    var pausaLonga = document.getElementById("longPause").value;
+    var rounds = document.getElementById("rounds").value;
+
+     
+    }
